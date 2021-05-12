eeb157cfcb9bd148cb807950bee8ade0dd09d268
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-12 18:13:58 -0300

    feat: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
-    handle (httpResponse: any): any {
-        return {
-            statusCode: 400,
-            body: new Error('Missing param: name')
+    handle(httpResponse: any): any {
+        if (!httpResponse.body.name) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: name')
+            }
+        }
+
+        if (!httpResponse.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: email')
+            }
         }
     }
 }
