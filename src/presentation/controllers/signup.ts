50e48a131a91b4f3bfa6bc9fefa91d2917d0479d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-12 22:49:43 -0300

    refactor: move all protocols to a index file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,5 @@
 import { badRequest, serverError } from "../helpers/http-helpers"
-import { HttpRequest, HttpResponse } from "../protocols/http"
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
+import { HttpRequest, HttpResponse, EmailValidator, Controller } from "../protocols"
 import { InvalidParamError, MissingParamError } from "../errors"
 
 export class SignUpController implements Controller {
