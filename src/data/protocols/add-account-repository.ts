7cccbbaea9bb8b2133718fb4bef181aea3be7057
Rian Negreiros <riannegreiros@gmail.com>
2021-05-16 03:38:51 -0300

    feat: ensure DAddAccount calls AddAccountReposity with correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel, AddAccountModel } from "../usercases/add-account/db-add-account-protocols";
+
+export interface AddAccountRepository {
+    add (accountData: AddAccountModel): Promise<AccountModel>
+}
