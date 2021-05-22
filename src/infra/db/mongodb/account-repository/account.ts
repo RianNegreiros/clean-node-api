eb365fb644eae3540b2fdcbe4507d3c4555f608c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-22 00:14:10 -0300

    chore: add express

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,7 @@
 import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/usercases/add-account";
-import { MongoHelper } from "../helpers/mongo-heleper";
+import { MongoHelper } from "../helpers/mongo-helper";
 
 export class AccountMongoRepository implements AddAccountRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
