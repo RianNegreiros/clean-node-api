e0fea43dd1c31c942733ee6077dfb33525a7091b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-24 18:25:49 -0300

    feat: ensure calls LoadAccountByEmailRepository wih correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from "../usercases/add-account/db-add-account-protocols";
+
+export interface LoadAccountByEmailRepository {
+    load (email: string): Promise<AccountModel>
+}
