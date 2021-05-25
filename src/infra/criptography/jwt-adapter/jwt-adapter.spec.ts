ff0992651673d27cd9a9937b58e56990c74f74fa
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-24 23:56:12 -0300

    test: ensure JwtAdapter returns a sign when success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,4 +8,10 @@ describe('Jwt Adapter', () => {
         await sut.encrypt('any_id')
         expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     })
+
+    test('Should return a token on sign success', async () => {
+        const sut = new JwtAdapter('secret')
+        const accessToken = await sut.encrypt('any_id')
+        expect(accessToken).toBe('any_token')
+    })
 })
