5d86131ec8096b55fc46d8781059cc591f484119
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-25 21:39:49 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let accountColletion: Collection
 
 describe('Signup Routes', () => {
 
@@ -13,12 +16,12 @@ describe('Signup Routes', () => {
     })
 
     beforeEach(async () => {
-        const accountColletion = await MongoHelper.getColletion('accounts')
+        accountColletion = await MongoHelper.getColletion('accounts')
         await accountColletion.deleteMany({})
     })
 
     describe('POST /signup', () => {
-        test('Should return an account on succes', async () => {
+        test('Should return an account on success', async () => {
             await request(app)
                 .post('/api/signup')
                 .send({
@@ -26,6 +29,18 @@ describe('Signup Routes', () => {
                     email: '',
                     password: '',
                     passwordConfirmation: '123'
+                })
+                .expect(200)
+        })
+    })
+
+    describe('POST /login', () => {
+        test('Should return 200 on login', async () => {
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: '',
+                    password: '',
                 })
                 .expect(200)
         })
