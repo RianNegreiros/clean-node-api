2f0242589a33a5ec4a4b5847501024914281e8c2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-27 19:31:20 -0300

    test: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,3 +1,4 @@
+import { AddSurvey, AddSurveyModel } from "./add-survey-protocols"
 import { badRequest } from "../../../helpers/http/http-helpers"
 import { HttpRequest } from "../../../protocols"
 import { Validation } from "../../../protocols/validation"
@@ -22,17 +23,29 @@ const makeValidation = (): Validation => {
     return new ValidationStub()
 }
 
+const makeAddSurvey = (): AddSurvey => {
+    class AddSurveyStub implements AddSurvey {
+        async add (data: AddSurveyModel): Promise<void> {
+            return new Promise(resolve => resolve())
+        }
+    }
+    return new AddSurveyStub()
+}
+
 interface SutTypes {
     sut: AddSurveyController
     validationStub: Validation
+    addSurveyStub: AddSurvey
 }
 
 const makeSut = (): SutTypes => {
     const validationStub = makeValidation()
-    const sut = new AddSurveyController(validationStub)
+    const addSurveyStub = makeAddSurvey()
+    const sut = new AddSurveyController(validationStub, addSurveyStub)
     return {
         sut,
-        validationStub
+        validationStub,
+        addSurveyStub
     }
 }
 
@@ -50,5 +63,13 @@ describe('AddSurvey Controller', () => {
         jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(badRequest(new Error()))
+    })
+
+    test('Should call AddSurvey with correct values', async () => {
+        const { sut, addSurveyStub } = makeSut()
+        const validateSpy = jest.spyOn(addSurveyStub, 'add')
+        const httpRequest = makeFakeRequest()
+        await sut.handle(httpRequest)
+        expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
     })
 })
