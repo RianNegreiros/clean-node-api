4173447bdc7c7cf06e0949f5bf0d597081719563
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-27 22:35:03 -0300

    refactor: login routes

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -2,9 +2,8 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
 import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
-import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decorator-factory'
 
 export default (router: Router): void => {
-    router.post('/signup', adaptRoute(makeLogControllerDecorator(makeSignUpController())))
-    router.post('/login', adaptRoute(makeLogControllerDecorator(makeLoginController())))
+    router.post('/signup', adaptRoute(makeSignUpController()))
+    router.post('/login', adaptRoute(makeLoginController()))
 }
