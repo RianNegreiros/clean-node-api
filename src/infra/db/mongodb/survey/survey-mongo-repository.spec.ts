d60ffc121fd5b41601a5e47522c246a4619d3438
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-27 22:16:56 -0300

    test: ensure SurveyMongoRepository add a new survey on add sucess

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,39 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let surveyColletion: Collection
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountColletion = await MongoHelper.getColletion('accounts')
+        accountColletion.deleteMany({})
+    })
+
+    const makeSut = (): SurveyMongoRepository => {
+        return new SurveyMongoRepository()
+    }
+
+    test('Should add a survey on success', async () => {
+        const sut = makeSut()
+        await sut.add({
+            question: 'any_question',
+            answers: [{
+                image: 'any_image',
+                answer: 'any_answer'
+            }, {
+                answer: 'other_answer'
+            }]
+        })
+        const survey = await surveyColletion.findOne({ question: 'any_question' })
+        expect(survey).toBeTruthy()
+    })
+})
