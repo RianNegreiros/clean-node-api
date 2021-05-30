0b62a356b32cf86feac1bcecfa2791c5da95bc97
Rian Negreiros <riannegreiros@gmail.com>
2021-05-29 23:59:54 -0300

    refactor: fix typo

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -79,7 +79,7 @@ describe('Account Mongo Repository', () => {
     })
 
     describe('loadByToken', () => {
-        test('Should return an account on loadByToken success', async () => {
+        test('Should return an account on loadByToken without role', async () => {
             const sut = makeSut()
             await accountCollection.insertOne({
                 name: 'any_name',
