505688c4b6a2b13ec5130c853966604ef1164eb2
Rian Negreiros <riannegreiros@gmail.com>
2021-06-01 21:06:07 -0300

    refactor: remove duplicated folders

--- src/main/factories/middleware/auth-middleware-factory.ts
+++
@@ -1,7 +0,0 @@
-import { AuthMiddleware } from '../../../presentation/middleware/auth-middleware'
-import { Middleware } from '../../../presentation/protocols'
-import { makeDbLoadAccountByToken } from '../userscases/account/load-account-by-factory/db-load-account-by-factory'
-
-export const makeAuthMiddleware = (role?: string): Middleware => {
-  return new AuthMiddleware(makeDbLoadAccountByToken(), role)
-}
